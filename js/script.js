f3801b48fa16d31e63320b50b00fcb6b20c8f0ad
Frank <100966778+FrankMen06@users.noreply.github.com>
2022-10-26 11:51:36 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-var nombres;
+function guardarDatos(){
+
+    let datoNombre = document.getElementById("nombres").value; 
+    document.getElementById("aleatorios").innerHTML = datoNombre; 
+
+
+    savearray = [];
+    var guardar = function(){
+        var array = [];                                 
+        for (var i = 0; i < 3; i++) {               
+            array[i] = (guardarDatos);
+            savearray.push (array[i]);  
+        }
+    }                       
+
+}
